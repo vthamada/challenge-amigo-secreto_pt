318824f5498aa8110acb6f9965d53d25a30ad9ce
Ricardo Hamada <vitor_hamada@yahoo.com.br>
2025-02-07 21:44:17 -0300

    Criando função adicionarAmigo()

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 let amigos = []
+
+function adicionarAmigo(){
+    let nomeAmigo = document.querySelector('input').value;
+    
+    if (nomeAmigo == '') {
+        alert('Por favor, insira um nome.');
+    } else {
+        amigos.push(nomeAmigo); 
+        document.querySelector('input').value = '';
+    }
+}
