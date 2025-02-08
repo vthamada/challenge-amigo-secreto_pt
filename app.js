e4aae4fe8433724ab9d94c49e310b582e617cc36
Ricardo Hamada <vitor_hamada@yahoo.com.br>
2025-02-08 12:41:38 -0300

    Criando função atualizaListaAmigos()

--- app.js
+++ app.js
@@ -8,5 +8,16 @@ function adicionarAmigo(){
     } else {
         amigos.push(nomeAmigo); 
         document.querySelector('input').value = '';
+        atualizarListaAmigos();
+    }
+}
+function atualizarListaAmigos(){
+    listaAmigos = document.getElementById('listaAmigos');
+    listaAmigos.innerHTML = '';
+
+    for (let amigo of amigos) {
+        let item = document.createElement('li');
+        item.textContent = amigo;
+        listaAmigos.appendChild(item);
     }
 }
